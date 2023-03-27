6b7b9a4999b65ce4bd6a354c39e9453660a0a238
ItsNiels99 <43269780+ItsNiels99@users.noreply.github.com>
2023-03-27 02:30:58 +0200

    temporary startpage

--- index.php
+++ index.php
@@ -1,16 +1,17 @@
 <?php
+//
 //Require functions for actions
 /*
 require_once "includes/actions.php";
 
-//Based on the existence of the GET parameter, 1 of the 2 functions will be called
+Based on the existence of the GET parameter, 1 of the 2 functions will be called
 if (!isset($_GET['id'])) {
     $data = getDishes();
 } else {
     $data = getDishDetails($_GET['id']);
 }
 
-//Set the header & output JSON so the client will know what to expect.
+Set the header & output JSON so the client will know what to expect.
 header("Content-Type: application/json");
 echo json_encode($data);
 exit;
@@ -26,65 +27,8 @@ exit;
     <link rel="stylesheet" href="includes/style.css">
 </head>
 <body>
-<nav class="navbar" role="navigation" aria-label="main navigation">
-    <div class="navbar-brand">
-        <a class="navbar-item" href="https://bulma.io">
-            <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28">
-        </a>
+<?php include 'includes/nav.php' ?>
 
-        <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
-            <span aria-hidden="true"></span>
-            <span aria-hidden="true"></span>
-            <span aria-hidden="true"></span>
-        </a>
-    </div>
-
-    <div id="navbarBasicExample" class="navbar-menu">
-        <div class="navbar-start">
-            <a class="navbar-item">
-                Home
-            </a>
-
-            <a class="navbar-item">
-                Documentation
-            </a>
-
-            <div class="navbar-item has-dropdown is-hoverable">
-                <a class="navbar-link">
-                    More
-                </a>
-
-                <div class="navbar-dropdown">
-                    <a class="navbar-item">
-                        About
-                    </a>
-                    <a class="navbar-item">
-                        Jobs
-                    </a>
-                    <a class="navbar-item">
-                        Contact
-                    </a>
-                    <hr class="navbar-divider">
-                    <a class="navbar-item">
-                        Report an issue
-                    </a>
-                </div>
-            </div>
-        </div>
-
-        <div class="navbar-end">
-            <div class="navbar-item">
-                <div class="buttons">
-                    <a class="button is-primary">
-                        <strong>Sign up</strong>
-                    </a>
-                    <a class="button is-light">
-                        Log in
-                    </a>
-                </div>
-            </div>
-        </div>
-    </div>
-</nav>
+<img src="includes/img/banner.img" alt="banner foto temp.">
 </body>
 </html>
